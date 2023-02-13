30b5cf0d422110904136ec8dbdf2b7e2c3be8496
Tringapps-Ranjithkumar <ananthula.kumar@tringapps.com>
2023-02-13 10:37:02 +0530

    Initial commit

---
+++ src/main/java/org/example/Hashmap.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class Hashmap {
+    public static void main(String[] args){
+        Logger l = Logger.getLogger("com.api.jar");
+        HashMap<Integer,String> map=new HashMap<>();
+        String s1= "Key-Value pairs: ";
+
+        map.put(4,"Grapes");
+        map.put(2,"Orange");
+        map.put(3,"Apple");
+        map.put(1,"JackFruit");
+
+        l.log(Level.INFO,()->"Size: " +map.size());
+        l.log(Level.INFO,()->"Values : " + map.values());
+        l.log(Level.INFO,()->"Keys : " + map.keySet());
+        l.log(Level.INFO,()-> s1 + map.entrySet());
+
+        l.log(Level.INFO,()-> "Getting a value"+map.get(2));
+
+        l.info("After removing ");
+        map.remove(2);
+        l.log(Level.INFO,()-> s1 +map.entrySet());
+
+        l.info("After replacing ");
+        map.replace(2,"Kiwi");
+        l.log(Level.INFO,()-> s1 + map.entrySet());
+    }
+}
